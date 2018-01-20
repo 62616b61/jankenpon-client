4c0b9e2c322f2e2c173e24270911aabc6344d0d2
whynotcrybot <alexanderkatsero@gmail.com>
2018-01-20 17:53:17 +0200

    socket.io-client + server negotiation logic

---
+++ src/js/api/socket.js
@@ -0,0 +1,18 @@
+import io from 'socket.io-client'
+const socket = io('http://localhost:3030')
+
+export function onStart (callback) {
+  socket.on('start', () => callback())
+}
+
+export function onAnnouncement (callback) {
+  socket.on('announce', result => callback(result))
+}
+
+export function emitReadiness () {
+  socket.emit('ready')
+}
+
+export function emitChoice (shape) {
+  socket.emit('choice', shape)
+}
--- src/js/pages/app.jsx
+++ src/js/pages/app.jsx
@@ -1,16 +1,89 @@
 import React from 'react'
 import { Flex, Box } from 'reflexbox'
+import {emitReadiness, emitChoice, onStart, onAnnouncement} from '../api/socket'
 
 import '../../style/index.global.css'
 
-const Root = (props) => {
-  return (
-    <Flex justify='center'>
-      <Box w={2 / 3}>
-        <p>Home</p>
-      </Box>
-    </Flex>
-  )
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      isReady: false,
+      didChoose: false,
+      opponentFound: false,
+      matchResult: null
+    }
+
+    onStart(() => this.setState({
+      opponentFound: true
+    }))
+
+    onAnnouncement(result => this.setState({
+      matchResult: result
+    }))
+  }
+
+  ready () {
+    emitReadiness()
+    this.setState({
+      isReady: true
+    })
+  }
+
+  choice (shape) {
+    emitChoice(shape)
+    this.setState({
+      didChoose: true
+    })
+  }
+
+  render () {
+    return (
+      <Flex
+        w={1}
+        align='center'
+        justify='center'
+        style={{
+          position: 'absolute',
+          height: '100%'
+        }}>
+          {
+            !this.state.isReady ? (
+              <button onClick={() => this.ready()}>I am ready!</button>
+            ) : (
+              this.state.opponentFound ? (
+                this.state.didChoose ? (
+                  this.state.matchResult ? (
+                    this.state.matchResult === 'win' ? (
+                      <h1>You win!</h1>
+                    )
+                    :
+                    this.state.matchResult === 'lose' ? (
+                      <h1>You lose!</h1>
+                    )
+                    :
+                    (
+                      <h1>Tie!</h1>
+                    )
+                  ) : (
+                    <h1>Your opponent is still thinking...</h1>
+                  )
+                ) : (
+                  <Box>
+                    <button onClick={() => this.choice(0)}>Rock!</button>
+                    <button onClick={() => this.choice(1)}>Paper!</button>
+                    <button onClick={() => this.choice(2)}>Scissors!</button>
+                  </Box>
+                )
+              ) : (
+                <h1>Finding your opponent...</h1>
+              )
+            )
+          }
+      </Flex>
+    )
+  }
 }
 
-export default Root
+export default App
